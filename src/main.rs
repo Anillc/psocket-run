70047f06e362381994c8977c716133e2862f3634
Anillc <i@anillc.cn>
2022-09-07 15:03:16 +0800

    fix bind

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use std::collections::HashMap;
 use std::convert::TryInto;
 use std::mem::size_of;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 use std::str::FromStr;
+use std::io::{Result, Error, ErrorKind};
 
-use nix::errno::errno;
 use nix::sys::signal::Signal;
 use nix::sys::wait::{waitpid, WaitStatus, WaitPidFlag};
 use nix::sys::ptrace;
@@ -31,28 +32,46 @@ struct Psocket<'a> {
     fwmark: Option<u32>,
     cidr: Option<(u128, u8)>,
     rng: &'a mut ThreadRng,
+    // pid, socket fd, pidfd socket fd
+    sockets: &'a mut HashMap<Pid, HashMap<i32, i32>>
 }
 
 const WALL: Option<WaitPidFlag> = Some(WaitPidFlag::__WALL);
 
 impl Psocket<'_> {
-    unsafe fn handle_syscall(&mut self, pid: Pid) {
-        let regs = ptrace::getregs(pid);
-        if regs.is_err() { return; }
-        let regs = regs.unwrap();
+    fn get_sockets(&mut self, pid: Pid) -> &mut HashMap<i32, i32> {
+        if !self.sockets.contains_key(&pid) {
+            self.sockets.insert(pid, HashMap::new());
+        }
+        self.sockets.get_mut(&pid).unwrap()
+    }
+
+    unsafe fn handle_syscall(&mut self, pid: Pid, enter: bool) -> Result<()> {
+        let regs = ptrace::getregs(pid)?;
         let orig_rax = regs.orig_rax as i64;
         let rax = regs.rax as i32;
         let rdi = regs.rdi as i32;
 
-        if rax < 0 { return; }
-
         match orig_rax {
             libc::SYS_socket => {
-                let pidfd = libc::syscall(libc::SYS_pidfd_open, pid.as_raw(), 0) as i32;
-                if pidfd < 0 { return; }
-                let pfd = libc::syscall(libc::SYS_pidfd_getfd, pidfd, rax, 0) as i32;
-                if pfd < 0 { return; }
+                if enter { return Ok(()); }
+                let pfd = get_fd(pid, rax)?;
                 if rdi == libc::AF_INET6 && self.cidr.is_some() {
+                    self.get_sockets(pid).insert(rax, pfd);
+                }
+                if let Some(fwmark) = self.fwmark {
+                    let mark = &fwmark as *const u32 as *const libc::c_void;
+                    libc::setsockopt(pfd, libc::SOL_SOCKET, libc::SO_MARK, mark, size_of::<u32>() as u32);
+                }
+            },
+            libc::SYS_close | libc::SYS_bind => {
+                if !enter { return Ok(()); }
+                self.get_sockets(pid).remove(&rdi);
+            },
+            libc::SYS_connect => {
+                if !enter || self.cidr.is_none() { return Ok(()); }
+                let fd = self.get_sockets(pid).remove(&rdi);
+                if let Some(fd) = fd {
                     let cidr = self.cidr.unwrap();
                     let addr = libc::in6_addr {
                         s6_addr: random_address(&cidr, self.rng).to_be_bytes(),
@@ -64,21 +83,13 @@ impl Psocket<'_> {
                         sin6_addr: addr,
                         sin6_scope_id: 0,
                     };
-                    libc::bind(pfd, sockaddr as *const libc::sockaddr, size_of::<libc::sockaddr_in6>() as u32);
-                    println!("{:?} {:?} {}", errno(), pid, pfd);
+                    libc::bind(fd, sockaddr as *const libc::sockaddr, size_of::<libc::sockaddr_in6>() as u32);
+
                 }
-                if let Some(fwmark) = self.fwmark {
-                    let mark = &fwmark as *const u32 as *const libc::c_void;
-                    libc::setsockopt(pfd, libc::SOL_SOCKET, libc::SO_MARK, mark, size_of::<u32>() as u32);
-                }
-            },
-            libc::SYS_bind => {
-                let mut new_regs = regs.clone();
-                new_regs.rax = 0;
-                ptrace::setregs(pid, regs).unwrap();
-            },
+            }
             _ => (),
         };
+        Ok(())
     }
 
     fn child(&mut self) {
@@ -99,20 +110,27 @@ impl Psocket<'_> {
             | ptrace::Options::PTRACE_O_TRACEVFORK;
         ptrace::setoptions(child, options).unwrap();
         ptrace::syscall(child, None).unwrap();
+        let mut enter = false;
         loop {
             let status = waitpid(None, WALL).unwrap();
             let pid = status.pid().unwrap();
             let mut signal: Option<Signal> = None;
+            enter = !enter;
             match status {
-                WaitStatus::PtraceSyscall(_) => unsafe { self.handle_syscall(pid) },
+                WaitStatus::PtraceSyscall(_) => {
+                    unsafe { self.handle_syscall(pid, enter).ok() };
+                },
                 WaitStatus::Exited(_, _) | WaitStatus::Signaled(_, _, _) => {
+                    enter = true;
+                    self.sockets.remove(&pid);
                     if pid == child { break; } else { continue; }
                 },
+                WaitStatus::PtraceEvent(_, _, _) => enter = true,
                 WaitStatus::Stopped(_, Signal::SIGSTOP) => (),
                 WaitStatus::Stopped(_, sig) => signal = Some(sig),
                 _ => (),
             };
-            ptrace::syscall(pid, signal).unwrap_or_default();
+            ptrace::syscall(pid, signal).ok();
         }
     }
 }
@@ -126,6 +144,16 @@ fn random_address((addr, length): &(u128, u8), rng: &mut ThreadRng) -> u128 {
         .collect::<Vec<u8>>();
     let random: [u8; 16] = [left, right].concat().try_into().unwrap();
     addr | u128::from_be_bytes(random)
+}
+
+fn get_fd(pid: Pid, raw_fd: i32) -> Result<i32> {
+    unsafe {
+        let pidfd = libc::syscall(libc::SYS_pidfd_open, pid.as_raw(), 0) as i32;
+        if pidfd < 0 { return Err(Error::new(ErrorKind::Other, "cannot get pidfd")); }
+        let fd = libc::syscall(libc::SYS_pidfd_getfd, pidfd, raw_fd, 0) as i32;
+        if fd < 0 { return Err(Error::new(ErrorKind::Other, "cannot get fd")); }
+        Ok(fd)
+    }
 }
 
 fn main() {
@@ -149,6 +177,7 @@ fn main() {
         fwmark,
         cidr,
         rng: &mut rand::thread_rng(),
+        sockets: &mut HashMap::new(),
     };
     match unsafe { fork() } {
         Ok(ForkResult::Child) => psocket.child(),
